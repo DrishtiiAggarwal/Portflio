d172ec409f79a7833642cfdfdda54ee72aa52403
Drishti Aggarwal <97733511+DrishtiiAggarwal@users.noreply.github.com>
2024-09-29 23:14:17 +0530

    routes-setup-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div>hello</div>
+
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Logo from "../Images/Logo.jpeg"
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
-}
+     <div className='flex'>
+      <div className='h-28 w-28 flex items-center'>
+        <img src={Logo}/> 
+      </div>
+      <div>
+        <Link to="/projects">Projects</Link>
+        <button>Updates</button>
+        <a target='_blank' href='https://drive.google.com/file/d/1luFzj2npcIQMtlmLuypZpZxDg2RFKwC9/view?usp=sharing'>Resume</a>
+        <Link to="/about">About</Link>
+
+      </div>
+     </div>
+);
+};
 
 export default Navbar
